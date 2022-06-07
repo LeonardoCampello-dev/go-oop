939155b8fb3e79586bb6b23e12a8707d67ed91bb
Leonardo Campello <leonardocampello.dev@gmail.com>
2022-06-07 07:27:11 -0300

    to separate checking account logic into a package

---
+++ accounts/checking-account.go
@@ -0,0 +1,46 @@
+package accounts
+
+type CheckingAccount struct {
+	Holder  string
+	Agency  int
+	Account int
+	Balance float64
+}
+
+func (account *CheckingAccount) Withdraw(value float64) (string, float64) {
+	isReadyToWithdraw := value > 0 && value <= account.Balance
+
+	if isReadyToWithdraw {
+		account.Balance -= value
+
+		return "Withdrawal successful!", account.Balance
+	}
+
+	return "Insufficient balance for withdrawal.", account.Balance
+}
+
+func (account *CheckingAccount) Deposit(value float64) (string, float64) {
+	isInvalidValue := value < 0
+
+	if isInvalidValue {
+		return "Invalid value", account.Balance
+	}
+
+	account.Balance += value
+
+	return "Deposit made successfully", account.Balance
+}
+
+func (account *CheckingAccount) Transfer(value float64, destinationAccount *CheckingAccount) (string, float64) {
+	haveEnoughBalance := value > 0 && value < account.Balance
+
+	if haveEnoughBalance {
+		account.Balance -= value
+
+		destinationAccount.Deposit(value)
+
+		return "Transfer performed successfully", account.Balance
+	}
+
+	return "Insufficient amount for transfer", account.Balance
+}
--- main.go
+++ main.go
@@ -1,66 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type CheckingAccount struct {
-	holder  string
-	agency  int
-	account int
-	balance float64
-}
+	"github.com/LeonardoCampello-dev/go-oop/accounts"
+)
 
 func main() {
-	firstAccount := CheckingAccount{
-		"Leonardo",
-		111,
-		1,
-		999.99,
+	firstAccount := accounts.CheckingAccount{
+		Holder:  "Leonardo",
+		Agency:  111,
+		Account: 1,
+		Balance: 9999.99,
 	}
 
-	secondAccount := CheckingAccount{
-		"Tester",
-		222,
-		2,
-		100,
-	}
-
-	fmt.Println(firstAccount.transfer(100, &secondAccount))
-}
-
-func (account *CheckingAccount) withdraw(value float64) (string, float64) {
-	isReadyToWithdraw := value > 0 && value <= account.balance
-
-	if isReadyToWithdraw {
-		account.balance -= value
-
-		return "Withdrawal successful!", account.balance
-	}
-
-	return "Insufficient balance for withdrawal.", account.balance
-}
-
-func (account *CheckingAccount) deposit(value float64) (string, float64) {
-	isInvalidValue := value < 0
-
-	if isInvalidValue {
-		return "Invalid value", account.balance
-	}
-
-	account.balance += value
-
-	return "Deposit made successfully", account.balance
-}
-
-func (account *CheckingAccount) transfer(value float64, destinationAccount *CheckingAccount) (string, float64) {
-	haveEnoughBalance := value > 0 && value < account.balance
-
-	if haveEnoughBalance {
-		account.balance -= value
-
-		destinationAccount.deposit(value)
-
-		return "Transfer performed successfully", account.balance
-	}
-
-	return "Insufficient amount for transfer", account.balance
+	fmt.Println(firstAccount)
 }
